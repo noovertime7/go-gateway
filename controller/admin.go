4549d194bdb15bd8c1f046de96452919b3276ed6
chenteng <cc17854330572@163.com>
2022-06-14 22:24:05 +0800

    feat:获取session，admin_info接口完成

---
+++ controller/admin.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/noovertime7/go-gateway/dto"
+	"github.com/noovertime7/go-gateway/middleware"
+	"github.com/noovertime7/go-gateway/public"
+)
+
+type AdminController struct {
+}
+
+func AdminRegister(group *gin.RouterGroup) {
+	admininfo := &AdminController{}
+	group.GET("/admin_info", admininfo.AdminInfo)
+}
+
+func (a *AdminController) AdminInfo(ctx *gin.Context) {
+	sess := sessions.Default(ctx)
+	sessinfo := sess.Get(public.AdminSessionInfoKey)
+	adminsessioninfo := &dto.AdminSessionInfo{}
+	if err := json.Unmarshal([]byte(fmt.Sprint(sessinfo)), adminsessioninfo); err != nil {
+		middleware.ResponseError(ctx, 3000, err)
+		return
+	}
+	//1、读取sessionkey对应的json，转换为结构体
+	//2、取出数据然后封装输出
+	out := &dto.AdminInfoOutput{
+		ID:           adminsessioninfo.ID,
+		Name:         adminsessioninfo.UserName,
+		LoginTime:    adminsessioninfo.LoginTime,
+		Avatar:       "",
+		Introduction: "我是介绍",
+		Rules:        []string{"admin"},
+	}
+	middleware.ResponseSuccess(ctx, out)
+}
--- middleware/session_auth.go
+++ middleware/session_auth.go
@@ -4,12 +4,13 @@ import (
 	"errors"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/noovertime7/go-gateway/public"
 )
 
 func SessionAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
-		if name, ok := session.Get("user").(string); !ok || name == "" {
+		if admininfo, ok := session.Get(public.AdminSessionInfoKey).(string); !ok || admininfo == "" {
 			ResponseError(c, InternalErrorCode, errors.New("user not login"))
 			c.Abort()
 			return
