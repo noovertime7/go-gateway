670b0bf30d942d040286a3fa5e38d41960056a32
chenteng <cc17854330572@163.com>
2022-06-23 14:36:12 +0800

    fix: 更换httpdetail jsonname为蛇形命名法

--- dao/service.go
+++ dao/service.go
@@ -2,9 +2,9 @@ package dao
 
 type ServiceDetail struct {
 	Info          *ServiceInfo   `json:"info" description:"基本信息"`
-	HttpRule      *HttpRule      `json:"http" description:"http"`
+	HttpRule      *HttpRule      `json:"http_rule" description:"http"`
 	TcpRule       *TcpRule       `json:"tcp"`
 	GRPCRule      *GrpcRule      `json:"grpc" description:"grpc"`
-	LoadBalance   *LoadBalance   `json:"loadbalance"`
-	AccessControl *AccessControl `json:"accesscontrol" description:"accesscontrol"`
+	LoadBalance   *LoadBalance   `json:"load_balance"`
+	AccessControl *AccessControl `json:"access_control" description:"accesscontrol"`
 }
